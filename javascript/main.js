acc279da4484c4dad1fbdea9c7ebab6a5754f6ce
Abin181881 <abinrv9@gmail.com>
2025-05-30 20:57:03 +0530

    new

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+document.querySelector(".enter").addEventListener("click" ,function(){
+  window.location.href="login.html";
+});
+document.querySelector(".ipo").addEventListener("click",function(){
+  window.location.href="ipo.html";
+})
